a407d3dea45bb75fd364e44bd7299339477c9213
shenjiahao <1843781563@qq.com>
2018-11-26 14:31:20 +0800

    Update ryu_start.sh, generate_topo_and_flow_cata.sh and sr_pusher_start.sh

--- ryu_start.sh
+++ ryu_start.sh
@@ -8,5 +8,5 @@ echo "##  wait and do not interrupt the process.     ##"
 echo "#################################################"
 
 WORKSPACE="workspace"
-cd /home/user/$WORKSPACE/dreamer-ryu/ryu/app
+cd $HOME/SDN/dreamer-ryu/ryu/app
 ryu-manager --observe-links ofctl_rest.py rest_topology
--- sr_pusher_start.sh
+++ sr_pusher_start.sh
@@ -13,8 +13,8 @@ echo "## ./sr_pusher_start.sh 10.255.245.1:8080 --add         ##"
 echo "##########################################################"
 
 WORKSPACE="workspace"
-cd /home/user/$WORKSPACE/sdn-te-sr-tools
-mv java-te-sr/flow/flow_catalogue.json.out OSHI-SR-pusher/out_flow_catalogue.json
-cd /home/user/$WORKSPACE/sdn-te-sr-tools/OSHI-SR-pusher/
+cd $HOME/SDN/SDN-TE-SR-tools
+mv java-te-sr/flow/out_flow_catalogue.json OSHI-SR-pusher/
+cd $HOME/SDN/SDN-TE-SR-tools/OSHI-SR-pusher/
 rm sr_vlls.json
 ./sr_vll_pusher.py --controller $1 $2
